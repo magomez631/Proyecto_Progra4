d4277dd85df7d97ffc13b1f3af0d8a0289b5e5b5
mhb20 <mhb1422@gmail.com>
2020-04-10 14:07:16 -0600

    Creacion de la forma login

---
+++ frmLogin.aspx
@@ -0,0 +1,60 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="frmLogin.aspx.cs" Inherits="Web.Interfaces.frmLogin" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous"/>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
+
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <div class="container mdi">
+        <form id="frmLogin" runat="server">
+
+            <div class="row">
+                <div class="col-md-2 celdas">
+                    <div class="form-group">
+                        <asp:Label Text="Usuario:" runat="server"></asp:Label>
+                    </div>
+                </div>
+
+                <div class="col-md-3 celdas">
+                    <div class="form-group">
+                        <asp:TextBox ID="txtUsuario" CssClass="form-control" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-2 celdas">
+                    <div class="form-group">
+                        <asp:Label Text="Contraseña:" runat="server"></asp:Label>
+                    </div>
+                </div>
+
+                <div class="col-md-3 celdas">
+                    <div class="form-group">
+                        <asp:TextBox ID="txtContrasena" TextMode="Password" CssClass="form-control" runat="server"></asp:TextBox>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row" style="margin-top: 25px;">
+                <div class="col-md-6 celdas" style="text-align:center">
+                    <asp:Button CssClass="btn btn-primary" ID="btnLogin" runat="server" Text="Ingresar" OnClick="btnLogin_Click" />
+                </div>
+            </div>
+        </form>
+
+    </div>
+</body>
+</html>
---
+++ frmLogin.aspx.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+//using System.Threading;
+using System.Diagnostics;
+
+namespace Web.Interfaces
+{
+    public partial class frmLogin : System.Web.UI.Page
+    {
+
+        #region "Variables"
+            string logname = "LogAppWebEmpleados";
+            string Origen  = "AppEventosLoginEmpleado";
+  
+        #endregion
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        void limpiarControles() 
+        
+        {
+            txtUsuario.Text = string.Empty;
+            txtContrasena.Text = string.Empty;
+            
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (!EventLog.Exists(logname))
+                {
+                    EventLog.CreateEventSource(Origen, logname);
+                }
+
+                if ((string.IsNullOrEmpty(txtUsuario.Text)) || (string.IsNullOrEmpty(txtContrasena.Text)))
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "Mensaje", "<script>swal('Atención', 'Favor ingresar usuario o clave', 'warning')</script>");
+
+                    return;
+                }
+
+               
+
+                Logica.Clases.clsLogin login = new Logica.Clases.clsLogin();
+                string mensaje = login.stLoginEmpleado(txtUsuario.Text, Convert.ToInt64(txtContrasena.Text));
+
+                limpiarControles();
+
+                string mensaje1 = "Usuario " + txtUsuario.Text + " ingresó correctamente";
+                string mensaje2 = "Usuario " + txtUsuario.Text + " trató de ingresar";
+
+                if (mensaje == "1") 
+                {
+                   
+
+                    if (EventLog.SourceExists(Origen)) 
+                    {
+                        EventLog.WriteEntry(Origen, mensaje1);
+                    }
+                    Response.Redirect("frmPrincipal.aspx");
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "Mensaje", "<script>swal('Atención', 'Usuario o clave incorrectos', 'warning')</script>"); if (EventLog.SourceExists(Origen))
+
+                        if (EventLog.SourceExists(Origen))
+                        {
+                            EventLog.WriteEntry(Origen, mensaje2);
+                        }
+
+
+
+
+                }
+
+            }
+            catch (Exception ) { throw; }
+        }
+    }
+}
